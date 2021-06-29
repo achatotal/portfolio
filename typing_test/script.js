674b4639178e7410e1b6c8dc6dcfceb2f56fc65f
Andrej Kucera <kuceraand@gmail.com>
2021-06-29 23:05:51 +0200

    json

--- typing_test/script.js
+++ typing_test/script.js
@@ -27,7 +27,7 @@ var text = texts[Math.floor(Math.random() * 3)]
 getData();
 async function getData(){
     const response = await fetch('texts.cvs');
-    const data = await JSON.parse(response.json());
+    const data = await response.json();
     console.log(data);
 }
 
