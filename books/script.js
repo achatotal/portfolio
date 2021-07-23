77789cd642f36ad69fbbce13f663b386f43c11ea
Andrej Kucera <kuceraand@gmail.com>
2021-07-23 13:17:50 +0200

    design

--- books/script.js
+++ books/script.js
@@ -28,4 +28,105 @@ document.addEventListener('DOMContentLoaded', function(){
         image.src = `media/smallBook/${i}.jpg`;
         smallBook.appendChild(image);
     }
+
+    // Hide photos
+    saroyan.style.display = 'none';
+    bigBook.style.display = 'none';
+    brezina.style.display = 'none';
+    smallBook.style.display = 'none';
+
+    document.getElementById('book1').addEventListener('click', function(){
+        if (saroyan.style.display === 'none'){
+            hidePictures()
+            hideText()
+            saroyan.style.display = 'block';
+            hideMenu();
+            document.getElementById('text1').style.display = 'block';
+            document.getElementById('menu1').style.top = '1rem';
+            document.body.scrollTop = saroyan.offsetTop-62;
+            document.documentElement.scrollTop = saroyan.offsetTop-62;
+
+        }
+        else {
+            hideText()
+            saroyan.style.display = 'none';
+        }
+    });
+
+    document.getElementById('book2').addEventListener('click', function(){
+        if (bigBook.style.display === 'none'){
+            hidePictures()
+            hideText()
+            bigBook.style.display = 'block';
+            hideMenu();
+            document.getElementById('text2').style.display = 'block';
+            document.getElementById('menu2').style.top = '1rem';
+            document.body.scrollTop = bigBook.offsetTop-62;
+            document.documentElement.scrollTop = bigBook.offsetTop-62;
+        }
+        else {
+            hideText()
+            bigBook.style.display = 'none';
+        }
+    });
+
+    document.getElementById('book3').addEventListener('click', function(){
+        if (brezina.style.display === 'none'){
+            hidePictures()
+            hideText()
+            brezina.style.display = 'block';
+            hideMenu();  
+            document.getElementById('text3').style.display = 'block';
+            document.getElementById('menu3').style.top = '1rem';
+            document.body.scrollTop = brezina.offsetTop-62;
+            document.documentElement.scrollTop = brezina.offsetTop-62;
+        }
+        else {
+            hideText()
+            brezina.style.display = 'none';
+        }
+    });
+
+    document.getElementById('book4').addEventListener('click', function(){
+        if (smallBook.style.display === 'none'){
+            hidePictures()
+            hideText()
+            smallBook.style.display = 'block';
+            hideMenu();
+            document.getElementById('text4').style.display = 'block';
+            document.getElementById('menu4').style.top = '1rem';
+            document.body.scrollTop = smallBook.offsetTop-62;
+            document.documentElement.scrollTop = smallBook.offsetTop-62;
+        }
+        else {
+            hideText()
+            smallBook.style.display = 'none';
+        }
+    });
 })
+
+function hideMenu() {
+    document.getElementById('menu1').style.top = '-50rem';
+    document.getElementById('menu2').style.top = '-50rem';
+    document.getElementById('menu3').style.top = '-50rem';
+    document.getElementById('menu4').style.top = '-50rem';
+}
+
+function hidePictures() {
+    document.getElementById('saroyan').style.display = 'none';
+    document.getElementById('bigBook').style.display = 'none';
+    document.getElementById('brezina').style.display = 'none';
+    document.getElementById('smallBook').style.display = 'none';
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+function hideText() {
+    document.getElementById('text1').style.display = 'none';
+    document.getElementById('text2').style.display = 'none';
+    document.getElementById('text3').style.display = 'none';
+    document.getElementById('text4').style.display = 'none';
+}
--- script.js
+++ script.js
@@ -2,60 +2,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // Change design If portfolio is viewed on ipad or phone
     if (window.innerWidth > 780){
-
-        // Typing Test
-        const typingTest = document.getElementById('typingTest');
-        typingTest.addEventListener('mouseover', function(){
-            this.style.backgroundImage = `url("design/bigDrawings/${(Math.floor(Math.random() * 1))}tt.png")`;
-        });
-        typingTest.addEventListener('mouseout', function(){
-            this.style.backgroundImage = 'none';
-        });
-
-        // Animace
-        const animace = document.getElementById('animace');
-        animace.addEventListener('mouseover', function(){
-            this.style.backgroundImage = `url("design/bigDrawings/animace.png")`;
-        });
-        animace.addEventListener('mouseout', function(){
-            this.style.backgroundImage = 'none';
-        });
-    
-        // Game of Life
-        const gameOfLife = document.getElementById('gameOfLife');
-        gameOfLife.addEventListener('mouseover', function(){
-            this.style.backgroundImage = `url("design/bigDrawings/gameoflife.png")`;
-        });
-        gameOfLife.addEventListener('mouseout', function(){
-            this.style.backgroundImage = 'none';
-        });
-    
-        // Drawing game
-        const drawingGame = document.getElementById('drawingGame');
-        drawingGame.addEventListener('mouseover', function(){
-            this.style.backgroundImage = 'url("design/bigDrawings/drawinggame.png")';
-        });
-        drawingGame.addEventListener('mouseout', function(){
-            this.style.backgroundImage = 'none';
-        });
-
-        // Books
-        const books = document.getElementById('books');
-        books.addEventListener('mouseover', function(){
-            this.style.background = `rgb(${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))})`;
-        });
-        books.addEventListener('mouseout', function(){
-            this.style.background = 'none';
-        });
-    
-        // Anthropocene
-        const anthropocene = document.getElementById('anthropocene');
-        anthropocene.addEventListener('mouseover', function(){
-            this.style.background = `rgb(${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))})`;
-        });
-        anthropocene.addEventListener('mouseout', function(){
-            this.style.background = 'none';
-        });
     
     }
     else {
@@ -74,3 +20,58 @@ document.addEventListener('DOMContentLoaded', function(){
         drawingGame.style.backgroundRepeat = 'no-repeat';
     }
 });
+
+
+ /*// Typing Test
+ const typingTest = document.getElementById('typingTest');
+ typingTest.addEventListener('mouseover', function(){
+     this.style.backgroundImage = `url("design/bigDrawings/${(Math.floor(Math.random() * 1))}tt.png")`;
+ });
+ typingTest.addEventListener('mouseout', function(){
+     this.style.backgroundImage = 'none';
+ });
+
+ // Animace
+ const animace = document.getElementById('animace');
+ animace.addEventListener('mouseover', function(){
+     this.style.backgroundImage = `url("design/bigDrawings/animace.png")`;
+ });
+ animace.addEventListener('mouseout', function(){
+     this.style.backgroundImage = 'none';
+ });
+
+ // Game of Life
+ const gameOfLife = document.getElementById('gameOfLife');
+ gameOfLife.addEventListener('mouseover', function(){
+     this.style.backgroundImage = `url("design/bigDrawings/gameoflife.png")`;
+ });
+ gameOfLife.addEventListener('mouseout', function(){
+     this.style.backgroundImage = 'none';
+ });
+
+ // Drawing game
+ const drawingGame = document.getElementById('drawingGame');
+ drawingGame.addEventListener('mouseover', function(){
+     this.style.backgroundImage = 'url("design/bigDrawings/drawinggame.png")';
+ });
+ drawingGame.addEventListener('mouseout', function(){
+     this.style.backgroundImage = 'none';
+ });
+
+ // Books
+ const books = document.getElementById('books');
+ books.addEventListener('mouseover', function(){
+     this.style.background = `rgb(${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))})`;
+ });
+ books.addEventListener('mouseout', function(){
+     this.style.background = 'none';
+ });
+
+ // Anthropocene
+ const anthropocene = document.getElementById('anthropocene');
+ anthropocene.addEventListener('mouseover', function(){
+     this.style.background = `rgb(${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))},${(Math.floor(Math.random() * 255))})`;
+ });
+ anthropocene.addEventListener('mouseout', function(){
+     this.style.background = 'none';
+ });*/
